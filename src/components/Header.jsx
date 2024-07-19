f517f8bc85fa61641f4202a037029fc6a1b38017
Franco Morales Castro <franco.morales@outlook.com>
2024-07-19 18:34:02 -0400

    feat: change counter link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
                 <nav>
                     <Link to="/" className="mr-4">Home</Link>
                     <Link to="/tasks/create" className="mr-4">Crear tarea</Link>
-                    <Link to="/contador" className="mr-4">Contador</Link>
+                    <Link to="/counter" className="mr-4">Contador</Link>
                     <Link to="/about" className="mr-4">Sobre nosotros...</Link>
                 </nav>
             </div>
